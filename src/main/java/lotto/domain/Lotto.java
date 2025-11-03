434c4f7f69933fcc3e403815115b5fb0a1d4509e
power <posmane5@gmail.com>
2025-11-03 19:26:55 +0900

    feat (domain) : 로또 생성 기능 및 검증기능 추가

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,33 @@
+package lotto.domain;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Lotto {
+    private final List<Integer> numbers;
+
+    //TODO: 로또 1개 생성
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
+    }
+
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        }
+
+        HashSet<Integer> numbersSet = new HashSet<>(numbers);
+        if (numbersSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+
+        boolean hasInvalidNumber = numbers.stream()
+                .anyMatch(number -> number > 45 || number < 1);
+        if (hasInvalidNumber) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1~45 입니다.");
+        }
+
+    }
+
+}
