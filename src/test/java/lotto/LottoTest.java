cd9d925595efee9402c74773eb7b7ad5eeee6dfc
power <posmane5@gmail.com>
2025-11-03 19:29:02 +0900

    test (domain) : Lotto 입력 숫자 검증 테스트 통과

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,5 +1,6 @@
 package lotto;
 
+import lotto.domain.Lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,4 +23,17 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호의 개수가 정확시 6개가 아니면 예외가 발생한다.")
+    @Test
+    void LottoNumeberCountTest() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 1~45 범위를 벗어나는 숫자가 하나라도 있으면 예외가 발생한다.")
+    @Test
+    void InvalidNumberTest() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
